b4e52f879f1af3eccba0f0b128fdf4d799c89501
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-28 21:31:59 -0300

    Feat: Criar classe produto para colocar o nome do produto, quantidade no estoque e o preço

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,36 @@
 public class Produto {
+    private String name;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+
+    public Produto(String name, double preco, int quantityInventory) {
+        this.name = name;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantityInventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void name(){
+        this.name = name;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double Preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void diminuirEstoque(int quantidade) {
+        this.quantidadeEmEstoque -= quantidade;
+    }
 }
